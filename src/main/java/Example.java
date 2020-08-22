bde4b531a6f57de905701734e2906347e7638280
Tim van der Graaff <timvdg45@gmail.com>
2020-08-22 09:35:17 -0500

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -12,6 +12,11 @@ public class Example {
     String home(@RequestParam(value = "person", defaultValue = "") String personName) {
         return "<h1>Hello " + personName + "!</h1>" ;
     }
+	
+	@RequestMapping(method = RequestMethod.GET)
+    String home(@RequestParam(value = "thing", defaultValue = "") String thisthing) {
+        return "<h1>It is " + thisthing + "!</h1>" ;
+    }
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(Example.class, args);
