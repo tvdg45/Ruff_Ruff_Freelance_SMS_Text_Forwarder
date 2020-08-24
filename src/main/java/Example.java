e178b187f0b82b5778bdd41db82218389d096260
Tim van der Graaff <timvdg45@gmail.com>
2020-08-24 13:27:00 -0500

    Update Example.java

--- src/main/java/Example.java
+++ src/main/java/Example.java
@@ -3,6 +3,7 @@ import org.springframework.boot.autoconfigure.*;
 import org.springframework.stereotype.*;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @EnableAutoConfiguration
 @RequestMapping("/")
