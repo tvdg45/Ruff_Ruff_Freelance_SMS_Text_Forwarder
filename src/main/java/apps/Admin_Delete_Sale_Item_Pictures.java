222138954bf72ee2dcca2c0aed9ab7d9b577e7ce
Tim van der Graaff <timvdg45@gmail.com>
2020-08-31 15:57:55 -0500

    Update Admin_Delete_Sale_Item_Pictures.java

--- src/main/java/apps/Admin_Delete_Sale_Item_Pictures.java
+++ src/main/java/apps/Admin_Delete_Sale_Item_Pictures.java
@@ -9,78 +9,42 @@ import java.io.PrintWriter;
 
 import java.sql.Connection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-public class Admin_Delete_Sale_Item_Pictures extends HttpServlet {
+@CrossOrigin(origins = "https://tdscloud-dev-ed--c.visualforce.com", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/admin-delete-sale-item-pictures")
+public class Admin_Delete_Sale_Item_Pictures {
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
-    }
-    
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@RequestMapping(method = RequestMethod.POST)
+    String home(
+		@RequestParam(value = "row_id", defaultValue = "") String row_id,
+		@RequestParam(value = "delete_sale_item_pictures", defaultValue = "") String delete_sale_item_pictures
+			   ) {
+		
+		Connection use_open_connection;
+		
+		use_open_connection = Config.openConnection();
         
-        //processRequest(request, response);
-
+		Control_Change_For_Sale_Items.use_connection = use_open_connection;
+		Control_Change_For_Sale_Items.row_id = row_id;
+		Control_Change_For_Sale_Items.delete_sale_item_pictures = delete_sale_item_pictures;
+		
+		if (delete_sale_item_pictures.equals("Delete picture")) {
+			
+			return Control_Change_For_Sale_Items.control_delete_sale_item_pictures();
+		} else {
+			
+			return "";
+		}
     }
-
-    /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        processRequest(request, response);
-  
-        response.addHeader("Access-Control-Allow-Origin", "https://tdscloud-dev-ed--c.visualforce.com");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  
-        PrintWriter out = response.getWriter();
-  
-        Connection use_open_connection;
-  
-        use_open_connection = Config.openConnection();
-        
-        if (String.valueOf(request.getParameter("delete_sale_item_pictures")).equals("Delete picture")) {
-            
-            Control_Change_For_Sale_Items.use_connection = use_open_connection;
-            Control_Change_For_Sale_Items.row_id = String.valueOf(request.getParameter("row_id"));
-            Control_Change_For_Sale_Items.delete_sale_item_pictures = String.valueOf(request.getParameter("delete_sale_item_pictures"));
-            
-            out.println(Control_Change_For_Sale_Items.control_delete_sale_item_pictures());
-        }
+	
+    public static void main(String[] args) throws Exception {
+		
+        SpringApplication.run(Admin_Delete_Sale_Item_Pictures.class, args);
     }
-
-    /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
-    
-    @Override
-    public String getServletInfo() {
-
-        return "Short description";
-    } // </editor-fold>
 }
