3b91f9dfa2c32d23d986f4f37a2c643cd69432e7
Tim van der Graaff <timvdg45@gmail.com>
2020-08-31 17:34:53 -0500

    Update Admin_Extract_Receipts.java

--- src/main/java/apps/Admin_Extract_Receipts.java
+++ src/main/java/apps/Admin_Extract_Receipts.java
@@ -8,76 +8,34 @@ import java.io.PrintWriter;
 
 import java.sql.Connection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-public class Admin_Extract_Receipts extends HttpServlet {
+@CrossOrigin(origins = "https://tdscloud-dev-ed--c.visualforce.com", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/admin-extract-receipts")
+public class Admin_Extract_Receipts {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	@RequestMapping(method = RequestMethod.POST)
+    String home() {
+		
+		Connection use_open_connection;
+		
+		use_open_connection = Config.openConnection();
+		
+		Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
+		
+		return "{\"receipts\": " +
+			Control_Change_Shopping_Cart_Items.control_search_receipts() + "," +
+			" \"items_sold\": " +
+			Control_Change_Shopping_Cart_Items.control_search_items_sold() + "}";
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        //processRequest(request, response);
-
+	
+    public static void main(String[] args) throws Exception {
+		
+        SpringApplication.run(Admin_Extract_Receipts.class, args);
     }
-
-    /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        processRequest(request, response);
-
-        response.addHeader("Access-Control-Allow-Origin", "https://tdscloud-dev-ed--c.visualforce.com");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        
-        PrintWriter out = response.getWriter();
-  
-        Connection use_open_connection;
-  
-        use_open_connection = configuration.Config.openConnection();
-  
-        Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
-        
-        out.println("{\"receipts\": ");
-        out.println(Control_Change_Shopping_Cart_Items.control_search_receipts() + ",");
-        out.println(" \"items_sold\": ");
-        out.println(Control_Change_Shopping_Cart_Items.control_search_items_sold() + "}");
-    }
-
-    /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
-    
-    @Override
-    public String getServletInfo() {
-
-        return "Short description";
-    } // </editor-fold>
 }
