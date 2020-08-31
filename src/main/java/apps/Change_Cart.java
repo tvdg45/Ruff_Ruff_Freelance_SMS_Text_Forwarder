ba7f76db338a91c65380ac3f8399e0711aed1838
Tim van der Graaff <timvdg45@gmail.com>
2020-08-31 18:42:07 -0500

    Update Change_Cart.java

--- src/main/java/apps/Change_Cart.java
+++ src/main/java/apps/Change_Cart.java
@@ -9,81 +9,48 @@ import java.io.PrintWriter;
 
 import java.sql.Connection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-public class Change_Cart extends HttpServlet {
+@CrossOrigin(origins = "https://www.timothysdigitalsolutions.com", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/change-cart")
+public class Change_Cart {
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
+	@RequestMapping(method = RequestMethod.POST)
+    String home(
+		@RequestParam(value = "row_id", defaultValue = "") String row_id,
+		@RequestParam(value = "quantity", defaultValue = "") String quantity,
+		@RequestParam(value = "guest_session", defaultValue = "") String guest_session,
+		@RequestParam(value = "item_id", defaultValue = "") String item_id,
+		@RequestParam(value = "change_cart", defaultValue = "") String change_cart
+			   ) {
+		
+		Connection use_open_connection;
+		
+		use_open_connection = Config.openConnection();
+		
+		Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
+		Control_Change_Shopping_Cart_Items.row_id = row_id.split(",");
+		Control_Change_Shopping_Cart_Items.quantity = quantity.split(",");
+		Control_Change_Shopping_Cart_Items.guest_session = guest_session;
+		Control_Change_Shopping_Cart_Items.item_id = item_id.split(",");
+		Control_Change_Shopping_Cart_Items.change_cart = change_cart;
+		
+		if (change_cart.equals("Update cart")) {
+			
+			return Control_Change_Shopping_Cart_Items.control_change_cart();
+		} else {
+			
+			return "";
+		}
     }
-    
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        //processRequest(request, response);
-
+	
+    public static void main(String[] args) throws Exception {
+		
+        SpringApplication.run(Change_Cart.class, args);
     }
-
-    /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        processRequest(request, response);
-  
-        response.addHeader("Access-Control-Allow-Origin", "https://www.timothysdigitalsolutions.com");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  
-        PrintWriter out = response.getWriter();
-  
-        Connection use_open_connection;
-  
-        use_open_connection = Config.openConnection();
-        
-        if (String.valueOf(request.getParameter("change_cart")).equals("Update cart")) {
-            
-            Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
-            Control_Change_Shopping_Cart_Items.row_id = String.valueOf(request.getParameter("row_id")).split(",");
-            Control_Change_Shopping_Cart_Items.quantity = String.valueOf(request.getParameter("quantity")).split(",");
-            Control_Change_Shopping_Cart_Items.guest_session = String.valueOf(request.getParameter("guest_session"));
-            Control_Change_Shopping_Cart_Items.item_id = String.valueOf(request.getParameter("item_id")).split(",");
-            Control_Change_Shopping_Cart_Items.change_cart = String.valueOf(request.getParameter("change_cart"));
-            
-            out.println(Control_Change_Shopping_Cart_Items.control_change_cart());
-        }
-    }
-
-    /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
-    
-    @Override
-    public String getServletInfo() {
-
-        return "Short description";
-    } // </editor-fold>
 }
