907175c7b174f706a12c81e7f0552e3553624b23
Tim van der Graaff <timvdg45@gmail.com>
2020-08-31 13:44:34 -0500

    Update Admin_Delete_Item_For_Sale.java

--- src/main/java/apps/Admin_Delete_Item_For_Sale.java
+++ src/main/java/apps/Admin_Delete_Item_For_Sale.java
@@ -9,78 +9,42 @@ import java.io.PrintWriter;
 
 import java.sql.Connection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-public class Admin_Delete_Item_For_Sale extends HttpServlet {
+@CrossOrigin(origins = "https://tdscloud-dev-ed--c.visualforce.com", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/admin-delete-item-for-sale")
+public class Admin_Delete_Item_For_Sale {
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
-    }
-    
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        //processRequest(request, response);
-
-    }
-
-    /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        processRequest(request, response);
-  
-        response.addHeader("Access-Control-Allow-Origin", "https://tdscloud-dev-ed--c.visualforce.com");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  
-        PrintWriter out = response.getWriter();
-  
-        Connection use_open_connection;
-  
-        use_open_connection = Config.openConnection();
-        
-        if (String.valueOf(request.getParameter("delete_item_for_sale")).equals("Delete item")) {
-            
-            Control_Change_For_Sale_Items.use_connection = use_open_connection;
-            Control_Change_For_Sale_Items.row_id = String.valueOf(request.getParameter("row_id"));
-            Control_Change_For_Sale_Items.delete_item_for_sale = String.valueOf(request.getParameter("delete_item_for_sale"));
-            
-            out.println(Control_Change_For_Sale_Items.control_delete_item_for_sale());
-        }
+	@RequestMapping(method = RequestMethod.POST)
+    String home(
+		@RequestParam(value = "row_id", defaultValue = "") String row_id,
+		@RequestParam(value = "delete_item_for_sale", defaultValue = "") String delete_item_for_sale
+			   ) {
+		
+		Connection use_open_connection;
+		
+		use_open_connection = Config.openConnection();
+		
+		Control_Change_For_Sale_Items.use_connection = use_open_connection;
+		Control_Change_For_Sale_Items.row_id = row_id;
+		Control_Change_For_Sale_Items.delete_item_for_sale = delete_item_for_sale;
+		
+		if (delete_item_for_sale.equals("Delete item")) {
+			
+			return Control_Change_For_Sale_Items.control_delete_item_for_sale();
+		} else {
+			
+			return "";
+		}
     }
 
-    /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
-    
-    @Override
-    public String getServletInfo() {
-
-        return "Short description";
-    } // </editor-fold>
+    public static void main(String[] args) throws Exception {
+		
+        SpringApplication.run(Admin_Delete_Item_For_Sale.class, args);
+    }
 }
