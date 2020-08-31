aa06248785c84946b7d75c4571686d33fe33e081
Tim van der Graaff <timvdg45@gmail.com>
2020-08-31 14:52:01 -0500

    Update Admin_Delete_Receipts.java

--- src/main/java/apps/Admin_Delete_Receipts.java
+++ src/main/java/apps/Admin_Delete_Receipts.java
@@ -8,78 +8,40 @@ import java.io.PrintWriter;
 
 import java.sql.Connection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-public class Admin_Delete_Receipts extends HttpServlet {
+@CrossOrigin(origins = "https://tdscloud-dev-ed--c.visualforce.com", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/admin-delete-receipts")
+public class Admin_Delete_Receipts {
 
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+	@RequestMapping(method = RequestMethod.POST)
+    String home(
+		@RequestParam(value = "delete_receipts", defaultValue = "") String delete_receipts
+			   ) {
+		
+		Connection use_open_connection;
+		
+		use_open_connection = Config.openConnection();
+		
+		Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
+		Control_Change_Shopping_Cart_Items.delete_receipts = delete_receipts;
+		
+		if (delete_receipts.equals("Delete receipts")) {
+			
+			return Control_Change_Shopping_Cart_Items.control_delete_receipts();
+		} else {
+			
+			return "";
+		}
     }
-
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        //processRequest(request, response);
-
+	
+    public static void main(String[] args) throws Exception {
+		
+        SpringApplication.run(Admin_Delete_Receipts.class, args);
     }
-
-    /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        processRequest(request, response);
-
-        response.addHeader("Access-Control-Allow-Origin", "https://tdscloud-dev-ed--c.visualforce.com");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-        
-        PrintWriter out = response.getWriter();
-  
-        Connection use_open_connection;
-  
-        use_open_connection = configuration.Config.openConnection();
-  
-        Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
-        
-        if (String.valueOf(request.getParameter("delete_receipts")).equals("Delete receipts")) {
-            
-            Control_Change_Shopping_Cart_Items.delete_receipts = request.getParameter("delete_receipts");
-            
-            out.println(Control_Change_Shopping_Cart_Items.control_delete_receipts());
-        }	
-    }
-
-    /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
-    
-    @Override
-    public String getServletInfo() {
-
-        return "Short description";
-    } // </editor-fold>
 }
