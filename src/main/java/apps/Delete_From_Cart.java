b3018bf5d4704a9df2ae0ae58f6ea8c61b1bc2a6
Tim van der Graaff <timvdg45@gmail.com>
2020-08-31 22:13:16 -0500

    Update Delete_From_Cart.java

--- src/main/java/apps/Delete_From_Cart.java
+++ src/main/java/apps/Delete_From_Cart.java
@@ -9,79 +9,44 @@ import java.io.PrintWriter;
 
 import java.sql.Connection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-public class Delete_From_Cart extends HttpServlet {
+@CrossOrigin(origins = "https://www.timothysdigitalsolutions.com", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/delete-from-cart")
+public class Delete_From_Cart {
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
-    }
-    
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@RequestMapping(method = RequestMethod.POST)
+    String home(
+		@RequestParam(value = "row_id", defaultValue = "") String row_id,
+		@RequestParam(value = "guest_session", defaultValue = "") String guest_session,
+		@RequestParam(value = "delete_from_cart", defaultValue = "") String delete_from_cart
+			   ) {
+		
+		Connection use_open_connection;
+		
+		use_open_connection = Config.openConnection();
         
-        //processRequest(request, response);
-
+		Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
+		Control_Change_Shopping_Cart_Items.row_id = row_id.split(",");
+		Control_Change_Shopping_Cart_Items.guest_session = guest_session;
+		Control_Change_Shopping_Cart_Items.delete_from_cart = delete_from_cart;
+		
+		if (delete_from_cart.equals("Delete item(s)")) {
+			
+			return Control_Change_Shopping_Cart_Items.control_delete_from_cart();
+		} else {
+			
+			return "";
+		}
     }
-
-    /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        processRequest(request, response);
-  
-        response.addHeader("Access-Control-Allow-Origin", "https://www.timothysdigitalsolutions.com");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  
-        PrintWriter out = response.getWriter();
-  
-        Connection use_open_connection;
-  
-        use_open_connection = Config.openConnection();
-        
-        if (String.valueOf(request.getParameter("delete_from_cart")).equals("Delete item(s)")) {
-            
-            Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
-            Control_Change_Shopping_Cart_Items.row_id = String.valueOf(request.getParameter("row_id")).split(",");
-            Control_Change_Shopping_Cart_Items.guest_session = String.valueOf(request.getParameter("guest_session"));
-            Control_Change_Shopping_Cart_Items.delete_from_cart = String.valueOf(request.getParameter("delete_from_cart"));
-            
-            out.println(Control_Change_Shopping_Cart_Items.control_delete_from_cart());
-        }
+	
+    public static void main(String[] args) throws Exception {
+		
+        SpringApplication.run(Delete_From_Cart.class, args);
     }
-
-    /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
-    
-    @Override
-    public String getServletInfo() {
-
-        return "Short description";
-    } // </editor-fold>
 }
