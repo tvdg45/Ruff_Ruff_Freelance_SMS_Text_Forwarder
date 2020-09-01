9b4ef1c66a59c258e040e1f8877be0a28a45299c
Tim van der Graaff <timvdg45@gmail.com>
2020-09-01 14:37:08 -0500

    Update Delete_Review.java

--- src/main/java/apps/Delete_Review.java
+++ src/main/java/apps/Delete_Review.java
@@ -9,79 +9,44 @@ import java.io.PrintWriter;
 
 import java.sql.Connection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-public class Delete_Review extends HttpServlet {
+@CrossOrigin(origins = "https://www.timothysdigitalsolutions.com", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/delete-review")
+public class Delete_Review {
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
+	@RequestMapping(method = RequestMethod.POST)
+    String home(
+		@RequestParam(value = "row_id", defaultValue = "") String row_id,
+		@RequestParam(value = "item_id", defaultValue = "") String item_id,
+		@RequestParam(value = "delete_review", defaultValue = "") String delete_review
+			   ) {
+		
+		Connection use_open_connection;
+		
+		use_open_connection = Config.openConnection();
+		
+		Control_Change_Reviews.use_connection = use_open_connection;
+		Control_Change_Reviews.row_id = row_id;
+		Control_Change_Reviews.item_id = item_id;
+		Control_Change_Reviews.delete_review = delete_review;
+		
+		if (delete_review.equals("Delete")) {
+			
+			return Control_Change_Reviews.control_delete_review();
+		} else {
+			
+			return "";
+		}
     }
-    
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        //processRequest(request, response);
-
+	
+    public static void main(String[] args) throws Exception {
+		
+        SpringApplication.run(Delete_Review.class, args);
     }
-
-    /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        processRequest(request, response);
-  
-        response.addHeader("Access-Control-Allow-Origin", "https://www.timothysdigitalsolutions.com");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  
-        PrintWriter out = response.getWriter();
-  
-        Connection use_open_connection;
-  
-        use_open_connection = Config.openConnection();
-        
-        if (String.valueOf(request.getParameter("delete_review")).equals("Delete")) {
-            
-            Control_Change_Reviews.use_connection = use_open_connection;
-            Control_Change_Reviews.row_id = String.valueOf(request.getParameter("row_id"));
-            Control_Change_Reviews.item_id = String.valueOf(request.getParameter("item_id"));
-            Control_Change_Reviews.delete_review = String.valueOf(request.getParameter("delete_review"));
-            
-            out.println(Control_Change_Reviews.control_delete_review());
-        }
-    }
-
-    /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
-    
-    @Override
-    public String getServletInfo() {
-
-        return "Short description";
-    } // </editor-fold>
 }
