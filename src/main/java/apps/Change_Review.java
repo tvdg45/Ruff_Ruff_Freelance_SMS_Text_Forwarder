cb70f720d5c12f9fe694d86615a3522552877fd4
Tim van der Graaff <timvdg45@gmail.com>
2020-08-31 20:56:53 -0500

    Update Change_Review.java

--- src/main/java/apps/Change_Review.java
+++ src/main/java/apps/Change_Review.java
@@ -10,90 +10,59 @@ import java.io.PrintWriter;
 
 import java.sql.Connection;
 
-import javax.servlet.ServletException;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+import org.springframework.stereotype.*;
+import org.springframework.web.bind.annotation.*;
 
-public class Change_Review extends HttpServlet {
+@CrossOrigin(origins = "https://www.timothysdigitalsolutions.com", maxAge = 3600)
+@RestController
+@EnableAutoConfiguration
+@RequestMapping("/change-review")
+public class Change_Review {
     
-    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    
-    }
-    
-    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /**
-    * Handles the HTTP <code>GET</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	@RequestMapping(method = RequestMethod.POST)
+    String home(
+		@RequestParam(value = "row_id", defaultValue = "") String row_id,
+		@RequestParam(value = "item_id", defaultValue = "") String item_id,
+		@RequestParam(value = "name", defaultValue = "") String name,
+		@RequestParam(value = "rating", defaultValue = "") String rating,
+		@RequestParam(value = "subject", defaultValue = "") String subject,
+		@RequestParam(value = "description", defaultValue = "") String description,
+		@RequestParam(value = "change_review", defaultValue = "") String change_review
+			   ) {
+		
+		Connection use_open_connection;
+		
+		use_open_connection = Config.openConnection();
+		
+		//Set this default string, so that it can be randomly scrambled in to a specified number of characters.
+		Security_Code_Generator.entire_string = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+		
+		//Specify the number of characters for the new security code.
+		Security_Code_Generator.number_of_characters = 25;
         
-        //processRequest(request, response);
-
+		Control_Change_Reviews.use_connection = use_open_connection;
+		Control_Change_Reviews.new_security_code = String.valueOf(Security_Code_Generator.generate_hash());
+		Control_Change_Reviews.row_id = row_id;
+		Control_Change_Reviews.item_id = item_id;
+		Control_Change_Reviews.name = name;
+		Control_Change_Reviews.rating = rating;
+		Control_Change_Reviews.subject = subject;
+		Control_Change_Reviews.description = description;
+		Control_Change_Reviews.change_review = change_review;
+		
+		if (change_review.equals("Submit")) {
+			
+			return Control_Change_Reviews.control_change_review();
+		} else {
+			
+			return "";
+		}
     }
-
-    /**
-    * Handles the HTTP <code>POST</code> method.
-    *
-    * @param request servlet request
-    * @param response servlet response
-    * @throws ServletException if a servlet-specific error occurs
-    * @throws IOException if an I/O error occurs
-    */
-    
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        
-        processRequest(request, response);
-  
-        response.addHeader("Access-Control-Allow-Origin", "https://www.timothysdigitalsolutions.com");
-        response.setHeader("Access-Control-Allow-Credentials", "true");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-  
-        PrintWriter out = response.getWriter();
-  
-        Connection use_open_connection;
-  
-        use_open_connection = Config.openConnection();
-        
-        if (String.valueOf(request.getParameter("change_review")).equals("Submit")) {
-            
-            //Set this default string, so that it can be randomly scrambled in to a specified number of characters.
-            Security_Code_Generator.entire_string = "1234567890abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            
-            //Specify the number of characters for the new security code.
-            Security_Code_Generator.number_of_characters = 25;
-            
-            Control_Change_Reviews.use_connection = use_open_connection;
-            Control_Change_Reviews.row_id = String.valueOf(request.getParameter("row_id"));
-            Control_Change_Reviews.item_id = String.valueOf(request.getParameter("item_id"));
-            Control_Change_Reviews.name = String.valueOf(request.getParameter("name"));
-            Control_Change_Reviews.rating = String.valueOf(request.getParameter("rating"));
-            Control_Change_Reviews.subject = String.valueOf(request.getParameter("subject"));
-            Control_Change_Reviews.description = String.valueOf(request.getParameter("description"));
-            Control_Change_Reviews.new_security_code = String.valueOf(Security_Code_Generator.generate_hash());
-            Control_Change_Reviews.change_review = String.valueOf(request.getParameter("change_review"));
-            
-            out.println(Control_Change_Reviews.control_change_review());
-        }
+	
+    public static void main(String[] args) throws Exception {
+		
+        SpringApplication.run(Change_Review.class, args);
     }
-
-    /**
-    * Returns a short description of the servlet.
-    *
-    * @return a String containing servlet description
-    */
-    
-    @Override
-    public String getServletInfo() {
-
-        return "Short description";
-    } // </editor-fold>
 }
