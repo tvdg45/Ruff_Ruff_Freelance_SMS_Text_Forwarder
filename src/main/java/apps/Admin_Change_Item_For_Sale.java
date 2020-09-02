abfa9cd0b8e9e4b1184f70ec23f51506da029491
Tim van der Graaff <timvdg45@gmail.com>
2020-09-02 17:54:21 -0500

    Update Admin_Change_Item_For_Sale.java

--- src/main/java/apps/Admin_Change_Item_For_Sale.java
+++ src/main/java/apps/Admin_Change_Item_For_Sale.java
@@ -5,7 +5,6 @@ import configuration.Config;
 import controllers.Control_Change_For_Sale_Items;
 
 import java.io.IOException;
-//import java.io.PrintWriter;
 
 import java.sql.Connection;
 
@@ -38,19 +37,19 @@ public class Admin_Change_Item_For_Sale {
 			
 			try {
 				
-			use_open_connection = Config.openConnection();
+				use_open_connection = Config.openConnection();
 				
-            Control_Change_For_Sale_Items.use_connection = use_open_connection;
-            Control_Change_For_Sale_Items.item = item;
-            Control_Change_For_Sale_Items.thumbnail = thumbnail;
-            Control_Change_For_Sale_Items.category = category.split(",");
-            Control_Change_For_Sale_Items.description = description;
-            Control_Change_For_Sale_Items.price = price;
-            Control_Change_For_Sale_Items.inventory = inventory;
-            Control_Change_For_Sale_Items.row_id = row_id;
-            Control_Change_For_Sale_Items.change_item_for_sale = change_item_for_sale;
+            	Control_Change_For_Sale_Items.use_connection = use_open_connection;
+            	Control_Change_For_Sale_Items.item = item;
+            	Control_Change_For_Sale_Items.thumbnail = thumbnail;
+            	Control_Change_For_Sale_Items.category = category.split(",");
+            	Control_Change_For_Sale_Items.description = description;
+            	Control_Change_For_Sale_Items.price = price;
+            	Control_Change_For_Sale_Items.inventory = inventory;
+            	Control_Change_For_Sale_Items.row_id = row_id;
+            	Control_Change_For_Sale_Items.change_item_for_sale = change_item_for_sale;
             
-            return Control_Change_For_Sale_Items.control_change_item_for_sale();
+            	return Control_Change_For_Sale_Items.control_change_item_for_sale();
 			} catch (IOException e) {
 				
 				return "";
