e1922f68cb238a2d8b59a3730386ec54aff7bbc0
Tim van der Graaff <timvdg45@gmail.com>
2020-09-02 23:07:17 -0500

    Update For_Sale_Items_Interface.java

--- src/main/java/apps/For_Sale_Items_Interface.java
+++ src/main/java/apps/For_Sale_Items_Interface.java
@@ -58,6 +58,6 @@ public class For_Sale_Items_Interface {
 	
     public static void main(String[] args) throws Exception {
 		
-        SpringApplication.run(apps.For_Sale_Items_Interface.class, args);
+        SpringApplication.run(For_Sale_Items_Interface.class, args);
     }   
 }
