ed54f8a847014c5974704dd2d9dd9e14d734babf
Tim van der Graaff <timvdg45@gmail.com>
2020-09-03 08:49:15 -0500

    Update Directory_Hider.java

--- src/main/java/Directory_Hider.java
+++ src/main/java/Directory_Hider.java
@@ -12,7 +12,7 @@ public class Directory_Hider {
 	@RequestMapping(method = RequestMethod.GET)
     String home() {
 		
-        return "Hello";
+        return "";
     }
 	
     public static void main(String[] args) throws Exception {
