fb75dfa6dc68a3752775f455e6d270c027085722
Tim van der Graaff <timvdg45@gmail.com>
2020-09-06 10:49:21 -0500

    Update Config.java

--- src/main/java/configuration/Config.java
+++ src/main/java/configuration/Config.java
@@ -20,6 +20,7 @@ import java.util.HashMap;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpEntity;
 import org.springframework.http.ResponseEntity;
 
 public class Config {
