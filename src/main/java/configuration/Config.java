6f1d515e1e50eed4ddbf79e4d63554afa275f007
Tim van der Graaff <timvdg45@gmail.com>
2020-09-06 10:50:46 -0500

    Update Config.java

--- src/main/java/configuration/Config.java
+++ src/main/java/configuration/Config.java
@@ -21,6 +21,7 @@ import org.springframework.web.client.RestTemplate;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpEntity;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 
 public class Config {
