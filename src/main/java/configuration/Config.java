80cf9fe648e0fb1d4e15b5f44dabe2c9cf837a1d
Tim van der Graaff <timvdg45@gmail.com>
2020-09-07 00:12:15 -0500

    Update Config.java

--- src/main/java/configuration/Config.java
+++ src/main/java/configuration/Config.java
@@ -63,28 +63,9 @@ HttpEntity<Map<String, Object>> entity = new HttpEntity<>(request_parameters, he
 // send POST request
 ResponseEntity<String> response = restTemplate.postForEntity(url_for_get_request, entity, String.class);
         
-        /*HttpURLConnection conection = (HttpURLConnection)url_for_get_request.openConnection();
-        conection.setRequestMethod("POST");
-        
-        int response_code = conection.getResponseCode();
-		
-		LOGGER.log(Level.INFO, "response code: " + response_code);*/
-		
 		LOGGER.log(Level.INFO, "credibility: " + response.getBody());
         
         if (response.getStatusCode() == HttpStatus.OK) {
-            
-            /*StringBuilder response;
-            
-            try (BufferedReader in = new BufferedReader(new InputStreamReader(conection.getInputStream()))) {
-                
-                response = new StringBuilder();
-                
-                while ((read_line = in.readLine()) != null) {
-                    
-                    response.append(read_line);
-                }
-            }*/
             
             String[] credentials = response.getBody().toString().split("\\s*,\\s*");
             
@@ -98,11 +79,12 @@ ResponseEntity<String> response = restTemplate.postForEntity(url_for_get_request
                     database_port = credentials[3];
                     database_name = credentials[4];
 					
-					database_url = "jdbc:mysql://" + database_server + ":" + database_port + "/" + database_name;
+					//database_url = "jdbc:mysql://" + database_server + ":" + database_port + "/" + database_name;
                     
-                    //database_url = "jdbc:mysql://" + database_server + ":" + database_port + "/timothys_digital_solutions_company_website";
+                    database_url = "jdbc:mysql://" + database_server + ":" + database_port + "/timothys_digital_solutions_company_website";
 					
-					LOGGER.log(Level.INFO, "database name: " + database_name + ", username: " + database_username + " password: " + database_password);
+					LOGGER.log(Level.INFO, "server: " + database_server + " username: " + database_username + " password: " + database_password +
+							  " port: " + database_port + " database name: " + database_name);
                 } catch (Exception e) {
                 }
             }
@@ -111,10 +93,6 @@ ResponseEntity<String> response = restTemplate.postForEntity(url_for_get_request
     
     public static Connection openConnection() throws IOException {
         
-		//call_database_information();
-		
-		//LOGGER.log(Level.INFO, "database connector: " + database_url + ", username: " + database_username + " password: " + database_password);
-		
         try {
             
             Class.forName("com.mysql.jdbc.Driver");
