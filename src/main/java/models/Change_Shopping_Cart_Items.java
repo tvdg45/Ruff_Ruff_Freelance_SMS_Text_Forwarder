a8cde0058b4aab8abc68094c8496ba2a1f88152f
Tim van der Graaff <timvdg45@gmail.com>
2020-09-08 16:20:37 -0500

    Update Change_Shopping_Cart_Items.java

--- src/main/java/models/Change_Shopping_Cart_Items.java
+++ src/main/java/models/Change_Shopping_Cart_Items.java
@@ -642,8 +642,6 @@ public class Change_Shopping_Cart_Items {
                 delete_statement.addBatch();
                 
                 records_to_delete++;
-				
-				LOGGER.log(Level.INFO, "deleted item from cart: " + Integer.parseInt(get_row_id()[i]));
             }
             
             if (records_to_delete > 0) {
