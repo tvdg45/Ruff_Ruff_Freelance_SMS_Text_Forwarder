493cb576fe489e7b37e79c0a78393ba0f9728d18
Tim van der Graaff <timvdg45@gmail.com>
2020-09-09 16:35:15 -0500

    Update Admin_Add_Sale_Item_Pictures.java

--- src/main/java/apps/Admin_Add_Sale_Item_Pictures.java
+++ src/main/java/apps/Admin_Add_Sale_Item_Pictures.java
@@ -24,42 +24,41 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin-add-sale-item-pictures")
 public class Admin_Add_Sale_Item_Pictures {
     
-	@RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     String home(
-		@RequestParam(value = "item_id", defaultValue = "") String item_id,
-		@RequestParam(value = "thumbnail", defaultValue = "") String thumbnail,
-		@RequestParam(value = "add_sale_item_pictures", defaultValue = "") String add_sale_item_pictures
-			   ) {
-		
-		Connection use_open_connection;
-		
-		try {
-			
-		use_open_connection = Config.openConnection();
+            @RequestParam(value = "item_id", defaultValue = "") String item_id,
+            @RequestParam(value = "thumbnail", defaultValue = "") String thumbnail,
+            @RequestParam(value = "add_sale_item_pictures", defaultValue = "") String add_sale_item_pictures
+    ) {
+        Connection use_open_connection;
         
-        DateTimeFormatter time_format = DateTimeFormatter.ofPattern("hh:mm a 'EST'");
-          
-        LocalDate localDate = LocalDate.now();
-        LocalTime localTime = LocalTime.now(ZoneId.of("America/New_York"));
-		
-		Control_Change_For_Sale_Items.use_connection = use_open_connection;
-		Control_Change_For_Sale_Items.item_id = item_id;
-		Control_Change_For_Sale_Items.thumbnail = thumbnail;
-		Control_Change_For_Sale_Items.date_received = String.valueOf(localDate);
-		Control_Change_For_Sale_Items.time_received = String.valueOf(time_format.format(localTime));
-		Control_Change_For_Sale_Items.add_sale_item_pictures = add_sale_item_pictures;
-		
-		if (add_sale_item_pictures.equals("Add picture")) {
-			
-			return Control_Change_For_Sale_Items.control_add_sale_item_pictures();
-		} else {
-			
-			return "";
-		}
-		} catch (IOException e) {
-			
-			return "";
-		}
+        try {
+            
+            use_open_connection = Config.openConnection();
+            
+            DateTimeFormatter time_format = DateTimeFormatter.ofPattern("hh:mm a 'EST'");
+            
+            LocalDate localDate = LocalDate.now();
+            LocalTime localTime = LocalTime.now(ZoneId.of("America/New_York"));
+            
+            Control_Change_For_Sale_Items.use_connection = use_open_connection;
+            Control_Change_For_Sale_Items.item_id = item_id;
+            Control_Change_For_Sale_Items.thumbnail = thumbnail;
+            Control_Change_For_Sale_Items.date_received = String.valueOf(localDate);
+            Control_Change_For_Sale_Items.time_received = String.valueOf(time_format.format(localTime));
+            Control_Change_For_Sale_Items.add_sale_item_pictures = add_sale_item_pictures;
+            
+            if (add_sale_item_pictures.equals("Add picture")) {
+                
+                return Control_Change_For_Sale_Items.control_add_sale_item_pictures();
+            } else {
+                
+                return "";
+            }
+        } catch (IOException e) {
+            
+            return "";
+        }
     }
 	
     public static void main(String[] args) throws Exception, IOException {
