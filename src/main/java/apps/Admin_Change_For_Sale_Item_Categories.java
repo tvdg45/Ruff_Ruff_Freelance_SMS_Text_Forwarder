ea44170dad94261c9acb9cb2b6ff1a059ebc6775
Tim van der Graaff <timvdg45@gmail.com>
2020-09-10 08:21:41 -0500

    Update Admin_Change_For_Sale_Item_Categories.java

--- src/main/java/apps/Admin_Change_For_Sale_Item_Categories.java
+++ src/main/java/apps/Admin_Change_For_Sale_Item_Categories.java
@@ -26,82 +26,82 @@ import org.springframework.web.bind.annotation.*;
 @EnableAutoConfiguration
 @RequestMapping("/admin-change-for-sale-item-categories")
 public class Admin_Change_For_Sale_Item_Categories {
-
-	@RequestMapping(method = RequestMethod.POST)
+    
+    @RequestMapping(method = RequestMethod.POST)
     String home(
-		@RequestParam(value = "category", defaultValue = "") String category,
-		@RequestParam(value = "external_id", defaultValue = "") String external_id,
-		@RequestParam(value = "add_categories", defaultValue = "") String add_categories,
-		@RequestParam(value = "change_categories", defaultValue = "") String change_categories,
-		@RequestParam(value = "delete_categories", defaultValue = "") String delete_categories
-			   ) {
-		
+            @RequestParam(value = "category", defaultValue = "") String category,
+            @RequestParam(value = "external_id", defaultValue = "") String external_id,
+            @RequestParam(value = "add_categories", defaultValue = "") String add_categories,
+            @RequestParam(value = "change_categories", defaultValue = "") String change_categories,
+            @RequestParam(value = "delete_categories", defaultValue = "") String delete_categories
+    ) {
         Connection use_open_connection;
         
-		try {
-        use_open_connection = Config.openConnection();
-        
-        DateTimeFormatter time_format = DateTimeFormatter.ofPattern("hh:mm a 'EST'");
-          
-        LocalDate localDate = LocalDate.now();
-        LocalTime localTime = LocalTime.now(ZoneId.of("America/New_York"));
-  
-        Control_Change_For_Sale_Items.use_connection = use_open_connection;
-        
-        String category_exception = "";
-        String external_id_exception = "";
-        
-        ArrayList<String> find = new ArrayList<>();
-        ArrayList<String> replace = new ArrayList<>();
-        
-        find.add("(");
-        find.add(")");
-        find.add("; ");
-        
-        replace.add("");
-        replace.add("");
-        replace.add(";");
-        
         try {
             
-            Control_Change_For_Sale_Items.category = Find_and_replace.find_and_replace(find, replace, category).split(";");
-        } catch (NullPointerException e) {
+            use_open_connection = Config.openConnection();
             
-            category_exception = "yes";
+            DateTimeFormatter time_format = DateTimeFormatter.ofPattern("hh:mm a 'EST'");
+            
+            LocalDate localDate = LocalDate.now();
+            LocalTime localTime = LocalTime.now(ZoneId.of("America/New_York"));
+            
+            Control_Change_For_Sale_Items.use_connection = use_open_connection;
+            
+            String category_exception = "";
+            String external_id_exception = "";
+            
+            ArrayList<String> find = new ArrayList<>();
+            ArrayList<String> replace = new ArrayList<>();
+            
+            find.add("(");
+            find.add(")");
+            find.add("; ");
+            
+            replace.add("");
+            replace.add("");
+            replace.add(";");
+            
+            try {
+                
+                Control_Change_For_Sale_Items.category = Find_and_replace.find_and_replace(find, replace, category).split(";");
+            } catch (NullPointerException e) {
+                
+                category_exception = "yes";
+            }
+            
+            try {
+                
+                Control_Change_For_Sale_Items.external_id = Find_and_replace.find_and_replace(find, replace, external_id).split(";");
+            } catch (NullPointerException e) {
+                
+                external_id_exception = "yes";
+            }
+            
+            Control_Change_For_Sale_Items.date_received = String.valueOf(localDate);
+            Control_Change_For_Sale_Items.time_received = String.valueOf(time_format.format(localTime));
+            
+            if (add_categories.equals("Add categories") && !(category_exception.equals("yes") || external_id_exception.equals("yes"))) {
+                
+                Control_Change_For_Sale_Items.control_add_sale_categories();
+            }
+            
+            if (change_categories.equals("Change categories") && !(category_exception.equals("yes") || external_id_exception.equals("yes"))) {
+                
+                Control_Change_For_Sale_Items.control_change_sale_categories();
+            }
+            
+            if (delete_categories.equals("Delete categories") && !(external_id_exception.equals("yes"))) {
+                
+                Control_Change_For_Sale_Items.control_delete_sale_categories();
+            }
+            
+            return "";
+        } catch (IOException e) {
+            
+            return "";
         }
-        
-        try {
-            
-            Control_Change_For_Sale_Items.external_id = Find_and_replace.find_and_replace(find, replace, external_id).split(";");
-        } catch (NullPointerException e) {
-            
-            external_id_exception = "yes";
-        }
-        
-        Control_Change_For_Sale_Items.date_received = String.valueOf(localDate);
-        Control_Change_For_Sale_Items.time_received = String.valueOf(time_format.format(localTime));
-        
-        if (add_categories.equals("Add categories") && !(category_exception.equals("yes") || external_id_exception.equals("yes"))) {
-            
-            Control_Change_For_Sale_Items.control_add_sale_categories();
-        }
-        
-        if (change_categories.equals("Change categories") && !(category_exception.equals("yes") || external_id_exception.equals("yes"))) {
-            
-            Control_Change_For_Sale_Items.control_change_sale_categories();
-        }
-        
-        if (delete_categories.equals("Delete categories") && !(external_id_exception.equals("yes"))) {
-            
-            Control_Change_For_Sale_Items.control_delete_sale_categories();
-        }
-		
-		return "";
-		} catch (IOException e) {
-			
-			return "";
-		}
-	}
+    }
 	
     public static void main(String[] args) throws Exception, IOException {
 		
