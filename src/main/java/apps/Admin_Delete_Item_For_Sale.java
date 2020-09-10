846c511110503775d3111f0cd8fc234f26bcd72c
Tim van der Graaff <timvdg45@gmail.com>
2020-09-10 08:32:37 -0500

    Update Admin_Delete_Item_For_Sale.java

--- src/main/java/apps/Admin_Delete_Item_For_Sale.java
+++ src/main/java/apps/Admin_Delete_Item_For_Sale.java
@@ -19,33 +19,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/admin-delete-item-for-sale")
 public class Admin_Delete_Item_For_Sale {
     
-	@RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     String home(
-		@RequestParam(value = "row_id", defaultValue = "") String row_id,
-		@RequestParam(value = "delete_item_for_sale", defaultValue = "") String delete_item_for_sale
-			   ) {
-		
-		Connection use_open_connection;
-		
-		try {
-			
-		use_open_connection = Config.openConnection();
-		
-		Control_Change_For_Sale_Items.use_connection = use_open_connection;
-		Control_Change_For_Sale_Items.row_id = row_id;
-		Control_Change_For_Sale_Items.delete_item_for_sale = delete_item_for_sale;
-		
-		if (delete_item_for_sale.equals("Delete item")) {
-			
-			return Control_Change_For_Sale_Items.control_delete_item_for_sale();
-		} else {
-			
-			return "";
-		}
-		} catch (IOException e) {
-			
-			return "";
-		}
+            @RequestParam(value = "row_id", defaultValue = "") String row_id,
+            @RequestParam(value = "delete_item_for_sale", defaultValue = "") String delete_item_for_sale
+    ) {
+        Connection use_open_connection;
+        
+        try {
+            
+            use_open_connection = Config.openConnection();
+            
+            Control_Change_For_Sale_Items.use_connection = use_open_connection;
+            Control_Change_For_Sale_Items.row_id = row_id;
+            Control_Change_For_Sale_Items.delete_item_for_sale = delete_item_for_sale;
+            
+            if (delete_item_for_sale.equals("Delete item")) {
+                
+                return Control_Change_For_Sale_Items.control_delete_item_for_sale();
+            } else {
+                
+                return "";
+            }
+        } catch (IOException e) {
+            
+            return "";
+        }
     }
 
     public static void main(String[] args) throws Exception, IOException {
