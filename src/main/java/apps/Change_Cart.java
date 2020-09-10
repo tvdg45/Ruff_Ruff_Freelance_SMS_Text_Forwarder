ea5397911597fa7398fd67da58ec1ba0b8ddde9e
Tim van der Graaff <timvdg45@gmail.com>
2020-09-10 09:06:15 -0500

    Update Change_Cart.java

--- src/main/java/apps/Change_Cart.java
+++ src/main/java/apps/Change_Cart.java
@@ -20,39 +20,38 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/change-cart")
 public class Change_Cart {
     
-	@RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     String home(
-		@RequestParam(value = "row_id", defaultValue = "") String row_id,
-		@RequestParam(value = "quantity", defaultValue = "") String quantity,
-		@RequestParam(value = "guest_session", defaultValue = "") String guest_session,
-		@RequestParam(value = "item_id", defaultValue = "") String item_id,
-		@RequestParam(value = "change_cart", defaultValue = "") String change_cart
-			   ) {
-		
-		Connection use_open_connection;
-		
-		try {
-			
-		use_open_connection = Config.openConnection();
-		
-		Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
-		Control_Change_Shopping_Cart_Items.row_id = row_id.split(",");
-		Control_Change_Shopping_Cart_Items.quantity = quantity.split(",");
-		Control_Change_Shopping_Cart_Items.guest_session = guest_session;
-		Control_Change_Shopping_Cart_Items.item_id = item_id.split(",");
-		Control_Change_Shopping_Cart_Items.change_cart = change_cart;
-		
-		if (change_cart.equals("Update cart")) {
-			
-			return Control_Change_Shopping_Cart_Items.control_change_cart();
-		} else {
-			
-			return "";
-		}
-		} catch (IOException e) {
-			
-			return "";
-		}
+            @RequestParam(value = "row_id", defaultValue = "") String row_id,
+            @RequestParam(value = "quantity", defaultValue = "") String quantity,
+            @RequestParam(value = "guest_session", defaultValue = "") String guest_session,
+            @RequestParam(value = "item_id", defaultValue = "") String item_id,
+            @RequestParam(value = "change_cart", defaultValue = "") String change_cart
+    ) {
+        Connection use_open_connection;
+        
+        try {
+            
+            use_open_connection = Config.openConnection();
+            
+            Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
+            Control_Change_Shopping_Cart_Items.row_id = row_id.split(",");
+            Control_Change_Shopping_Cart_Items.quantity = quantity.split(",");
+            Control_Change_Shopping_Cart_Items.guest_session = guest_session;
+            Control_Change_Shopping_Cart_Items.item_id = item_id.split(",");
+            Control_Change_Shopping_Cart_Items.change_cart = change_cart;
+            
+            if (change_cart.equals("Update cart")) {
+                
+                return Control_Change_Shopping_Cart_Items.control_change_cart();
+            } else {
+                
+                return "";
+            }
+        } catch (IOException e) {
+            
+            return "";
+        }
     }
 	
     public static void main(String[] args) throws Exception, IOException {
