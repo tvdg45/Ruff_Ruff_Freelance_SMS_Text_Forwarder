293aeaf4970efd5e7ba480afaa6401b4c7c89215
Tim van der Graaff <timvdg45@gmail.com>
2020-09-10 09:13:16 -0500

    Update Clear_All_Carts.java

--- src/main/java/apps/Clear_All_Carts.java
+++ src/main/java/apps/Clear_All_Carts.java
@@ -19,33 +19,32 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/clear-all-carts")
 public class Clear_All_Carts {
     
-	@RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST)
     String home(
-		@RequestParam(value = "raw_time_received", defaultValue = "") String raw_time_received,
-		@RequestParam(value = "clear_all_carts", defaultValue = "") String clear_all_carts
-			   ) {
-		
-		Connection use_open_connection;
-		
-		try {
-			
-		use_open_connection = Config.openConnection();
+            @RequestParam(value = "raw_time_received", defaultValue = "") String raw_time_received,
+            @RequestParam(value = "clear_all_carts", defaultValue = "") String clear_all_carts
+    ) {
+        Connection use_open_connection;
         
-		Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
-		Control_Change_Shopping_Cart_Items.raw_time_received = raw_time_received;
-		Control_Change_Shopping_Cart_Items.clear_all_carts = clear_all_carts;
-		
-		if (clear_all_carts.equals("Clear all carts")) {
-			
-			return Control_Change_Shopping_Cart_Items.control_clear_all_carts();
-		} else {
-			
-			return "";
-		}
-		} catch (IOException e) {
-			
-			return "";
-		}
+        try {
+            
+            use_open_connection = Config.openConnection();
+            
+            Control_Change_Shopping_Cart_Items.use_connection = use_open_connection;
+            Control_Change_Shopping_Cart_Items.raw_time_received = raw_time_received;
+            Control_Change_Shopping_Cart_Items.clear_all_carts = clear_all_carts;
+            
+            if (clear_all_carts.equals("Clear all carts")) {
+                
+                return Control_Change_Shopping_Cart_Items.control_clear_all_carts();
+            } else {
+                
+                return "";
+            }
+        } catch (IOException e) {
+            
+            return "";
+        }
     }
 	
     public static void main(String[] args) throws Exception, IOException {
