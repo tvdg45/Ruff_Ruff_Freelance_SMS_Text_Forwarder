846ace2e925d1455d1d930e31c99caf5c8587081
Tim van der Graaff <timvdg45@gmail.com>
2021-05-18 09:17:57 -0500

    Update Search_Email_Address.java

--- src/main/java/apps/Search_Email_Address.java
+++ src/main/java/apps/Search_Email_Address.java
@@ -48,9 +48,9 @@ public class Search_Email_Address extends HttpServlet {
 
         PrintWriter out = response.getWriter();
   
-        //Connection use_open_connection;
+        Connection use_open_connection;
   
-        //use_open_connection = configuration.Config.openConnection();
+        use_open_connection = configuration.Config.openConnection();
   
         String first_name = String.valueOf(request.getParameter("first_name"));
         String last_name = String.valueOf(request.getParameter("last_name"));
