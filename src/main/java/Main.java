825a7e9e337cef0cb5a9f27706718ab97a007337
Tim van der Graaff <timvdg45@gmail.com>
2021-06-13 18:22:17 -0500

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Object[] sources = new Object[2];
         
         sources[0] = Directory_Hider.class;
-        sources[1] = apps.Search_Email_Address.class;
+        sources[1] = apps.Send_Email_To_Vendor.class;
 		
         SpringApplication.run(sources, args);
     }
