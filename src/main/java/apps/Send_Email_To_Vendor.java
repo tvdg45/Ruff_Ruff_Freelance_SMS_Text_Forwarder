15cb290c69befe3ea1a74011bda09c3edfc2edda
Tim van der Graaff <timvdg45@gmail.com>
2021-06-19 17:09:58 -0500

    Update Send_Email_To_Vendor.java

--- src/main/java/apps/Send_Email_To_Vendor.java
+++ src/main/java/apps/Send_Email_To_Vendor.java
@@ -4,9 +4,6 @@ package apps;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import java.util.ArrayList;
 
 import utilities.Find_and_replace;
@@ -282,8 +279,6 @@ public class Send_Email_To_Vendor extends HttpServlet {
 				
 				for (int i = 0; i < each_vendor_text_email.length; i++) {
 					
-					LOGGER.log(Level.INFO, "Email: " + each_vendor_text_email[i]);
-					LOGGER.log(Level.INFO, "Port: " + port);
 					try {
 						
 						Properties props = new Properties();
@@ -324,107 +319,6 @@ public class Send_Email_To_Vendor extends HttpServlet {
 				out.println("email not sent");
 			}
 		}
-  
-        /*try {
-              
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "timothysdigitalsolutions.com");
-        props.put("mail.smtp.port", "290");
-        props.put("mail.debug", "true");
-		props.put("mail.smtp.auth", "true");
-        //props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-			
-		Authenticator auth = new Authenticator() {
-			//override the getPasswordAuthentication method
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("timothys@timothysdigitalsolutions.com", "Ranger12!");
-			}
-		};
-        
-        Session session = Session.getDefaultInstance(props, auth);
-        MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("timothys@timothysdigitalsolutions.com"));
-		//DestinationPhoneNumber@sms.ipipi.com
-        message.setRecipient(RecipientType.TO, new InternetAddress("ltrman1996@hotmail.com"));
-        message.setSubject("Notification");
-        message.setContent("<h1>Successful!</h1>", "text/html;charset=UTF-8"); // as "text/plain"
-        message.setSentDate(new Date());
-        Transport.send(message);
-  
-            out.println("Done");
-  
-        } catch (MessagingException e) {
-            out.println(e.getMessage());
-        }
-		
-        try {
-              
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "timothysdigitalsolutions.com");
-        props.put("mail.smtp.port", "290");
-        props.put("mail.debug", "true");
-		props.put("mail.smtp.auth", "true");
-        //props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-			
-		Authenticator auth = new Authenticator() {
-			//override the getPasswordAuthentication method
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("timothys@timothysdigitalsolutions.com", "Ranger12!");
-			}
-		};
-        
-        Session session = Session.getDefaultInstance(props, auth);
-        MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("timothys@timothysdigitalsolutions.com"));
-		//DestinationPhoneNumber@sms.ipipi.com
-        message.setRecipient(RecipientType.TO, new InternetAddress("2175086775@vtext.com"));
-        message.setSubject("Notification");
-        message.setContent("<h1>Successful!</h1>", "text/html;charset=UTF-8"); // as "text/plain"
-        message.setSentDate(new Date());
-        Transport.send(message);
-  
-            out.println("Done");
-  
-        } catch (MessagingException e) {
-            out.println(e.getMessage());
-        }
-		
-        try {
-              
-        Properties props = new Properties();
-        props.put("mail.smtp.host", "timothysdigitalsolutions.com");
-        props.put("mail.smtp.port", "290");
-        props.put("mail.debug", "true");
-		props.put("mail.smtp.auth", "true");
-        //props.put("mail.smtp.starttls.enable", "true");
-		props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
-			
-		Authenticator auth = new Authenticator() {
-			//override the getPasswordAuthentication method
-			protected PasswordAuthentication getPasswordAuthentication() {
-				return new PasswordAuthentication("timothys@timothysdigitalsolutions.com", "Ranger12!");
-			}
-		};
-        
-        Session session = Session.getDefaultInstance(props, auth);
-        MimeMessage message = new MimeMessage(session);
-        message.setFrom(new InternetAddress("timothys@timothysdigitalsolutions.com"));
-		//DestinationPhoneNumber@sms.ipipi.com
-        message.setRecipient(RecipientType.TO, new InternetAddress("2175086775@txt.att.net"));
-        message.setSubject("Notification");
-        message.setContent("<h1>Successful!</h1>", "text/html;charset=UTF-8"); // as "text/plain"
-        message.setSentDate(new Date());
-        Transport.send(message);
-  
-            out.println("Done");
-  
-        } catch (MessagingException e) {
-            out.println(e.getMessage());
-        }
-        
-      out.println("<br />yes 6");*/
     }
     
     /**
